07dddfd1aa7d228a08c12286da288bb33b3b3790
Micahmaeee <66246634+Micahmaeee@users.noreply.github.com>
2020-08-27 22:56:22 +0800

    Add files via upload

---
+++ mywebby.php
@@ -0,0 +1,36 @@
+<html> 
+<title>My Webby</title>
+<link rel="stylesheet" href="mycss.css">
+<center>
+<div class="box1">
+  <div class="box2">
+  <div class ="box3">
+
+  </div>
+</div>
+</div>
+<div class ="circle"></div>
+<h1>Eclipse</h1>
+</center>
+</div>
+<center>
+  <div class="navigator">
+  <a href="yey">HOME</a>
+  <a href="yey">ABOUT</a>
+  <a href="yey">MENU</a>
+  <a href="yey">COMMENTS</a>
+  </center>
+  <body>
+    <div>
+    
+      <img src="https://i.postimg.cc/q7rq49nv/wan.jpg" />
+      <img src="https://i.postimg.cc/8kYCjFW3/toh.jpg" />
+      <img src="https://i.postimg.cc/668vjxfk/treh.jpg" />
+      </div>
+<hr>
+<hr><center>
+<div class="footer">micahmaeee0@gmail.com</div>
+  </center>
+
+</body>
+  </html> 
